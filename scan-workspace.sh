1b5f9e0025d2cd7b5d5ac42b3c8f2fd19b397875
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-10-27 18:56:32 -0700

    fix up the scan a bit; ignore the reports

--- scan-workspace.sh
+++ scan-workspace.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 TAG="$(date +%F.%s)"
-TOP="${1:-~/Google_Drive/Workspace}"
+TOP="${1:-${HOME}/Google_Drive/Workspace}"
 REPORT="workspace-scan-${TAG}.md"
 
 echo "# Repo Scan for ${TOP}" | tee "${REPORT}"
 
-for i in $(find . -type d -name .git | sed -e 's/.git//' | sort);
+for i in $(find "${TOP}" -type d -name .git | sed -e 's/.git//' | sort);
 do
 
     echo "## ${i}"
-    if [ -d "${TOP}/${i}" ];
-    then
-        cd "${TOP}/${i}" || true
+    cd "${i}" || true
 
-        echo '```'
-        origin=$(git remote get-url --all origin)
-        repo=$(git remote get-url --all origin | gsed -e 's|:|/|' -e 's|git@|https://|' -e 's|\.com\-[^/]\+|.com|' -e 's|\.git$||')
-        echo "Origin: ${origin}"
-        echo "Web UI: ${repo}"
-        echo '```'
+    echo '```'
+    origin=$(git remote get-url --all origin)
+    repo=$(git remote get-url --all origin | gsed -e 's|:|/|' -e 's|git@|https://|' -e 's|\.com\-[^/]\+|.com|' -e 's|\.git$||')
+    echo "Origin: ${origin}"
+    echo "Web UI: ${repo}"
+    echo '```'
 
-        echo
+    echo
 
-        echo '```'
-        git status
-        echo '```'
+    echo '```'
+    git status
+    echo '```'
 
-        echo
-    else
-        echo "ERROR: Could not change directories to ${i}"
-    fi
-
+    echo
 done | tee -a "${REPORT}"
